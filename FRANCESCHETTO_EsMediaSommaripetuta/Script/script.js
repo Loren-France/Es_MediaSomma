b3a69fe45e6ab00c1422e4aa39676fe8f1f8dae2
FRANCESCHETTO LORENZO <14529846@itisrossi.vi.it>
2025-12-09 08:47:35 +0100

    4o aggiornamento

--- FRANCESCHETTO_EsMediaSommaripetuta/Script/script.js
+++ FRANCESCHETTO_EsMediaSommaripetuta/Script/script.js
@@ -7,10 +7,27 @@ for (let i=0;i<q;i++) {
     num[i]=prompt("Inserire il "+(i+1)+"o numero");
 }
 
-let somma=somma();
+let somma=somma(num);
 
+let media=media(somma,num);
 
+document.getElementById("output").innerHTML = "La somma dei numeri inseriti è: "+somma+"<br>"+"La media dei nuemri inseirti è: "+media;
 
-document.getElementById("output").innerHTML = output;
+}
 
+function somma(num) {
+let somm=0;
+
+    for (let i=0;i<num.lenght;i++) {
+        somm=somm+num[i];
+    }
+
+    return somm;
+}
+
+function media(somma,num) {
+
+    let media=somma/num.lenght;
+
+    return media;
 }
